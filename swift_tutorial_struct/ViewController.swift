541896f745eb7114f29cedbfe0369d2b4f40aa5e
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-30 01:26:48 +0900

    Initial Commit

---
+++ swift_tutorial_struct/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_tutorial_struct
+//
+//  Created by 早川マイケル on 2021/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
